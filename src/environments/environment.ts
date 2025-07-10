3b4be0f50f6d9c26497fa5fca3cc2371f3b07b02
norberthl <n.turek@healthlabs.pl>
2025-07-10 16:03:15 +0200

    update angular storefront for ecom demo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   medusaBackendUrl: 'http://localhost:9000',
-  medusaPublishableKey: 'pk_7bdd36c0442d036e69d28089327c15590bcc790722a4833b587c763a4a85bee4',
+  medusaPublishableKey: 'pk_387cefe6a8168838292c26e8c037dba126d25f690ea317000b3745b6060a8747',
   stripePublishableKey: 'pk_test_51RikdR2adRs3zbgvUPfXPo3KgOTMkJQYPFZl3ZNhb1XJQVl2m7rQJixvWF0y1xenlPFy3m4SU6DarYd1qEEYgVRr00SCQUDAxO'
 }; 
