a382db0db52ed9b3ac158a92bc83bbfe4f1a2625
Norbert Turek <n.turek@vibecompany.io>
2025-07-27 21:19:03 +0200

    add environments to gitignore

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  medusaBackendUrl: 'http://localhost:9000',
-  medusaPublishableKey: 'pk_387cefe6a8168838292c26e8c037dba126d25f690ea317000b3745b6060a8747',
-  stripePublishableKey: 'pk_test_51RikdR2adRs3zbgvUPfXPo3KgOTMkJQYPFZl3ZNhb1XJQVl2m7rQJixvWF0y1xenlPFy3m4SU6DarYd1qEEYgVRr00SCQUDAxO'
-}; 
